4307913d635094ca71819c801c6a97a025c08ae7
Nurbolat <nurba.dav@gmail.com>
2023-08-09 14:32:41 +0600

    changed System.err.println to printStackTrace

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,7 +2,6 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
@@ -30,15 +29,8 @@ public class FileReader {
                 profile.setEmail(params[2]);
             if (params[3] != null)
                 profile.setPhone(Long.valueOf(params[3]));
-        } catch (NumberFormatException e) {
-//            do nothing
-//            System.err.println("error during conversion of age or phone" + e.getMessage());
-        } catch (FileNotFoundException e) {
-//            do nothing
-//            System.err.println("file not found" + e.getMessage());
-        } catch (IOException e) {
-//            do nothing
-//            System.err.println("some kind of io exception I guess" + e.getMessage());
+        } catch (NumberFormatException | IOException e) {
+            e.printStackTrace();
         }
         return profile;
     }
